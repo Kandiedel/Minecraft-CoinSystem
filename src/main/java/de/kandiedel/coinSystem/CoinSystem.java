a1a92209afcbd97357a7505e7e972aa4445e968e
Kandiedel <triebelfinn@gmail.com>
2025-10-19 22:01:07 +0200

    noTicket: enable and disable message added

--- src/main/java/de/kandiedel/coinSystem/CoinSystem.java
+++ src/main/java/de/kandiedel/coinSystem/CoinSystem.java
@@ -1,16 +1,41 @@
 package de.kandiedel.coinSystem;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class CoinSystem extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        getServer().getConsoleSender().sendMessage("");
+        getServer().getConsoleSender().sendMessage(ChatColor.WHITE + "============================================");
+        getServer().getConsoleSender().sendMessage("");
+        getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[ ✔ ] "
+                + ChatColor.DARK_PURPLE + getDescription().getName()
+                + ChatColor.GRAY + " successfully enabled!"
+                + ChatColor.GREEN + " [ ✔ ]");
+        getServer().getConsoleSender().sendMessage("        "
+                + ChatColor.GRAY + "Developed By "
+                + ChatColor.DARK_PURPLE + "Kandiedel " + ChatColor.GRAY + "⚒ ");
+        getServer().getConsoleSender().sendMessage("");
+        getServer().getConsoleSender().sendMessage(ChatColor.WHITE + "============================================");
+        getServer().getConsoleSender().sendMessage("");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getServer().getConsoleSender().sendMessage("");
+        getServer().getConsoleSender().sendMessage(ChatColor.WHITE + "=============================================");
+        getServer().getConsoleSender().sendMessage("");
+        getServer().getConsoleSender().sendMessage(ChatColor.RED + "[ ✖ ] "
+                + ChatColor.DARK_PURPLE + getDescription().getName()
+                + ChatColor.GRAY + " successfully disabled!"
+                + ChatColor.RED + " [ ✖ ]");
+        getServer().getConsoleSender().sendMessage("        "
+                + ChatColor.GRAY + "Developed By "
+                + ChatColor.DARK_PURPLE + "Kandiedel " + ChatColor.GRAY + "⚒ ");
+        getServer().getConsoleSender().sendMessage("");
+        getServer().getConsoleSender().sendMessage(ChatColor.WHITE + "=============================================");
+        getServer().getConsoleSender().sendMessage("");
     }
 }
