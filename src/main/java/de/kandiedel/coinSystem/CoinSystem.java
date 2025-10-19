9033f56c34c89411b218a34d4bc35685b54e5a86
Kandiedel <triebelfinn@gmail.com>
2025-10-19 21:50:15 +0200

    Minecraft-CoinSystem

---
+++ src/main/java/de/kandiedel/coinSystem/CoinSystem.java
@@ -0,0 +1,17 @@
+package de.kandiedel.coinSystem;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CoinSystem extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
